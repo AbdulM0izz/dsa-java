be6376dc60c8875f0a256dda2e6e0f9c62cbc20b
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-07-28 13:36:20 +0500

    nested loops and workflow

---
+++ loops.java
@@ -0,0 +1,30 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // for loop
+        // for (initialization; condition ; updation ){}
+
+//        for (int i = 1; i <= 10; i++ || i = i+1 ) {
+//            System.out.println(i);
+//        }
+
+//        for (int i=1; i <= 5; i=i+1) {
+//            System.out.println("Moiz");
+//        }
+
+        // Nested for loop
+//        for ( int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 1; i <= 3; i++){
+//            for (int j=1; j<=3; j++){
+//                System.out.println("I = " + i + " j = " + j);
+//            }
+//        }
+
+    }
+}

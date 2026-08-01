3080f8dec28a47f869c74b37e19739088e53b2ce
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-08-01 13:00:40 +0500

    solid square pattern

---
+++ patterns.java
@@ -0,0 +1,29 @@
+public class patterns {
+
+    public static void main(String[] args) {
+
+        // Solid square pattern
+         int n = 4;
+
+         for ( int row = 1; row <= n; row++) {
+             // for column inner loop
+             for (int column = 1; column <= n; column++) {
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+

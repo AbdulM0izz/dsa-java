95269737d0f37b20353531eb7cbd54ea0099a488
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-08-02 12:11:58 +0500

    solid rhombus and more patterns

--- patterns.java
+++ patterns.java
@@ -3,15 +3,51 @@ public class patterns {
     public static void main(String[] args) {
 
         // Solid square pattern
-         int n = 4;
+//         int n = 4;
+//
+//         for ( int row = 1; row <= n; row++) {
+//             // for column inner loop
+//             for (int column = 1; column <= n; column++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
 
-         for ( int row = 1; row <= n; row++) {
-             // for column inner loop
-             for (int column = 1; column <= n; column++) {
-                 System.out.print("* ");
-             }
-             System.out.println();
-         }
+
+        // Solid rectangle pattern 2
+//        int n = 4;
+//        for ( int rows = 1; rows <= n; rows ++){
+//            for (int column = 1; column <= 5; column++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // Right angle triangle
+//        int n = 5;
+//        for (int rows= 1; rows<=5; rows++){
+//            for (int columns=1; columns<=rows; columns++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+        // Solid rhombus
+        int n = 5;
+        for (int rows=1; rows <= n; rows++){
+            // first spaces
+            for(int columns = 1; columns <= n-rows; columns++){
+                System.out.print(" ");
+            }
+            for (int columns = 1; columns <= n; columns++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
 
 
 

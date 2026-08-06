eafad3c3bd28c18c5ba4f07bd3c513b0862b995c
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-08-06 12:29:04 +0500

    Hollow Rectangle

--- patterns.java
+++ patterns.java
@@ -72,6 +72,27 @@ public class patterns {
 
 
 
+ // Hollow rectangle  n = 4;
+
+        int n = 4;
+        for (int row = 1; row <=n; row++) {
+            //columns
+            for (int columns = 1; columns<=6; columns++) {
+
+                if (row == 1 || row == n){
+                    System.out.print("* ");
+                } else {
+                    if (columns == 1 || columns == 6) {
+                        System.out.print("* ");
+                    }  else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+
 
 
 

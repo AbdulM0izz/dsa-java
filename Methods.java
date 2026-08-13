5a1c781fd774833c1b233f5310bc9d60cf3b8f34
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-08-14 01:12:52 +0500

    first method invoke

---
+++ Methods.java
@@ -0,0 +1,17 @@
+public class Methods {
+
+    // first methods print2kaTable
+
+    static void print2KaTable() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(2 * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // methods call / invoke a method
+        System.out.println("Hi Moiz");
+        print2KaTable();
+        System.out.println("Byeee");
+    }
+}

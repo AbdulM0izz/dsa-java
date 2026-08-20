2a15a60534ec1695c91baf6c0eed34cb717da2ef
AbdulM0izz <abdulmoiz95972@gmail.com>
2026-08-20 20:16:31 +0500

    practice problems for arrays

--- arrays.java
+++ arrays.java
@@ -5,11 +5,46 @@ public class arrays {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         // int arr [];  or int [] arr;  decleration
 
 //        int arr[] ={1,2,4};
-          int arr[] = new int[3];
+//          int arr[] = new int[3];
+
+
+        // Find max element in the array
+//        int arr[] = {5,4,3,2};
+//        int n = arr.length;
+//        int max = arr[0];
+//
+//        for ( int i=0; i<=n-1; i++) {
+//            if (arr[i] > max){
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println("Max in the array is " + max);
+
+        // multiply all the elements of the array
+//        int num[] = {2,3,10,20};
+//        int n = num.length;
+//        int prod  = 1;
+//
+//        for (int i=0; i<=n-1; i++) {
+//            int value = num[i];
+//            prod  = prod * value;
+//        }
+//        System.out.println("Product of the array is " + prod);
+
+        // Print sum of all the elements of arrays
+
+//        int num[] = {20,40,50,60};
+//        int n = num.length;
+//        int sum = 0;
+//
+//        for (int i = 0; i <= n-1; i++) {
+//            int value = num[i];
+//            sum += value;
+//        }
+//        System.out.println("Sum of the elements of this arrays is " + sum);
 
         // access array elements
 //        System.out.println("Values at index 0 is " + arr[0]);
@@ -28,12 +63,12 @@ public class arrays {
 //        }
 
         // take input in array and print the values
-        for (int index = 0; index <= 2; index++) {
-            arr[index] = input.nextInt();
-        }
-        for (int val : arr) {
-            System.out.println(val);
-        }
+//        for (int index = 0; index <= 2; index++) {
+//            arr[index] = input.nextInt();
+//        }
+//        for (int val : arr) {
+//            System.out.println(val);
+//        }
 
     }
 }
